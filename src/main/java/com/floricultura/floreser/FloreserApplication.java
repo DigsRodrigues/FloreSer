d852a46c77ae261408e2e6e4c697c16cf4768e07
DigsRodrigues <diegohrodrigues.993@gmail.com>
2025-09-22 09:03:49 -0300

    first commit

---
+++ src/main/java/com/floricultura/floreser/FloreserApplication.java
@@ -0,0 +1,13 @@
+package com.floricultura.floreser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloreserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloreserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/floricultura/floreser/FloreserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floricultura.floreser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloreserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
